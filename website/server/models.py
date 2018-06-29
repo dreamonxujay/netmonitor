0498cf4186ac72eeaaabfca32a27cccabdf0d0b8
barbedops <39816795+barbedops@users.noreply.github.com>
2018-06-29 15:21:37 +0800

    Add files via upload

--- website/server/models.py
+++ website/server/models.py
@@ -6,8 +6,60 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+
+class Hosts(models.Model):
+	ID = models.AutoField(blank=False, primary_key=True)  
+	IPADDR = models.GenericIPAddressField(protocol='IPv4', unpack_ipv4=False)
+	PROJ_AREA = models.CharField(max_length=40)
+	OPERATOR = models.CharField(max_length=40)
+	PROJ_NAME = models.CharField(max_length=200)
+	PRODUCT_NAME = models.CharField(max_length=200)
+	PROVINCE_NAME = models.CharField(max_length=20)
+	IDC_NAME = models.CharField(max_length=200)
+	PROJ_MANAGER = models.CharField(max_length=40)
+	PROJ_PERSON_IN_CHARGE = models.CharField(max_length=40)
+	PROJ_PERSON_IN_CHARGE_TEL = models.IntegerField(max_length=11)
+	PROJ_PERSON_IN_CHARGE_WECHAT = models.CharField(max_length=20)
+	EQPT_STATUS = models.CharField(max_length=20)
+	EQPT_DESC = models.CharField(max_length=300)
+
+class Ports(models.Model):
+	ID = models.AutoField(blank=False, primary_key=True)  
+	# HOSTS_ID = models.ForeignKey(Hosts, on_delete=models.CASCADE)
+	POSTS = models.IntegerField(max_length=5)
+	POSTS_DESC = models.CharField(max_length=2000)
+	RULE_ID = models.CharField(max_length=300)  ### 忘记意思了
+
+
+class History(models.Model):
+	DATE = models.DateTimeField(decimal_places=2) # 小数点后两位
+	HOSTS_ID = models.CharField(max_length=40)
+	PROTS_ID  = models.CharField(max_length=40)
+	SCAN_TYPE = models.CharField(max_length=40)
+	STATUS = models.CharField(max_length=40)
+	DETAILS = models.CharField(max_length=40)
+
+class Alerts(models.Model):
+	ID = models.AutoField(blank=False, primary_key=True)  
+	DATE = models.DateTimeField(decimal_places=2) # 小数点后两位
+	HOSTS_ID = models.CharField(max_length=40)
+	POSTS_ID = models.CharField(max_length=40)
+	STATUS = models.CharField(max_length=40)
+	DETAILS = models.CharField(max_length=40)
+
+class Rules(models.Model):
+	ID = models.AutoField(blank=False, primary_key=True)  
+	DESC = models.CharField(max_length=40)
+	RULE = models.CharField(max_length=40)
+
+class Alert_logs(models.Model):
+	ID = models.AutoField(blank=False, primary_key=True)  
+	AL_ID = models.CharField(max_length=40)
+	STATUS = models.CharField(max_length=40)
+	DATE = models.DateTimeField(decimal_places=2) # 小数点后两位
+	DETAILS = models.CharField(max_length=40)
