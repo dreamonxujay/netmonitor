d17fb77d7edcefaff7fd42e461e103822efeb8ed
barbedops <barbedops@163.com>
2018-07-03 18:37:38 +0800

    update

--- website/server/models.py
+++ website/server/models.py
@@ -3,43 +3,43 @@ from django.db import models
 #
 class Hosts(models.Model):
 	ID = models.AutoField(blank=False, primary_key=True)  
-	IPADDR = models.GenericIPAddressField(protocol='IPv4', unpack_ipv4=False)
-	PROJ_AREA = models.CharField(max_length=40,null=True,blank=True)
-	OPERATOR = models.CharField(max_length=40)
-	PROJ_NAME = models.TextField(max_length=200)
-	PRODUCT_NAME = models.TextField(max_length=200)
-	PROVINCE_NAME = models.CharField(max_length=20)
-	IDC_NAME = models.CharField(max_length=200)
-	PROJ_MANAGER = models.CharField(max_length=40)
-	PROJ_PERSON_IN_CHARGE = models.CharField(max_length=40)
-	PROJ_PERSON_IN_CHARGE_TEL = models.IntegerField()
-	PROJ_PERSON_IN_CHARGE_WECHAT = models.CharField(max_length=20)
-	EQPT_STATUS = models.CharField(max_length=20)
-	EQPT_DESC = models.TextField(max_length=300)
+	IPADDR = models.GenericIPAddressField(protocol='IPv4', unpack_ipv4=False, null=True, blank=True)
+	PROJ_AREA = models.CharField(max_length=40, null=True, blank=True)
+	OPERATOR = models.CharField(max_length=40, null=True, blank=True)
+	PROJ_NAME = models.TextField(max_length=200, null=True, blank=True)
+	PRODUCT_NAME = models.TextField(max_length=200, null=True, blank=True)
+	PROVINCE_NAME = models.CharField(max_length=20, null=True, blank=True)
+	IDC_NAME = models.CharField(max_length=200, null=True, blank=True)
+	PROJ_MANAGER = models.CharField(max_length=40, null=True, blank=True)
+	PROJ_PERSON_IN_CHARGE = models.CharField(max_length=40, null=True, blank=True)
+	PROJ_PERSON_IN_CHARGE_TEL = models.IntegerField(null=True, blank=True)
+	PROJ_PERSON_IN_CHARGE_WECHAT = models.CharField(max_length=20, null=True, blank=True)
+	EQPT_STATUS = models.CharField(max_length=20, null=True, blank=True)
+	EQPT_DESC = models.TextField(max_length=300, null=True, blank=True)
 
 class Ports(models.Model):
 	ID = models.AutoField(blank=False, primary_key=True)  
 	# HOSTS_ID = models.ForeignKey(Hosts, on_delete=models.CASCADE)
-	POSTS = models.IntegerField()
-	POSTS_DESC = models.TextField(max_length=2000)
-	RULE_ID = models.CharField(max_length=300)  
+	POSTS = models.IntegerField(null=True, blank=True)
+	POSTS_DESC = models.TextField(max_length=2000, null=True, blank=True)
+	RULE_ID = models.CharField(max_length=300, null=True, blank=True)  
 
 
 class History(models.Model):
-	DATE = models.DateTimeField(auto_now = True) # 字段保存时会自动保存当前时间
-	HOSTS_ID = models.CharField(max_length=40)
-	PROTS_ID  = models.CharField(max_length=40)
-	SCAN_TYPE = models.CharField(max_length=40)
-	STATUS = models.CharField(max_length=40)
-	DETAILS = models.CharField(max_length=40)
+	DATE = models.DateTimeField(auto_now = True, null=True, blank=True) # 字段保存时会自动保存当前时间
+	HOSTS_ID = models.CharField(max_length=40, null=True, blank=True)
+	PROTS_ID  = models.CharField(max_length=40, null=True, blank=True)
+	SCAN_TYPE = models.CharField(max_length=40, null=True, blank=True)
+	STATUS = models.CharField(max_length=40, null=True, blank=True)
+	DETAILS = models.CharField(max_length=40, null=True, blank=True)
 
 class Alerts(models.Model):
 	ID = models.AutoField(blank=False, primary_key=True)  
-	DATE = models.DateTimeField(auto_now = True) # 字段保存时会自动保存当前时间
-	HOSTS_ID = models.CharField(max_length=40)
-	POSTS_ID = models.CharField(max_length=40)
-	STATUS = models.CharField(max_length=40)
-	DETAILS = models.CharField(max_length=40)
+	DATE = models.DateTimeField(auto_now = True, null=True, blank=True) # 字段保存时会自动保存当前时间
+	HOSTS_ID = models.CharField(max_length=40, null=True, blank=True)
+	POSTS_ID = models.CharField(max_length=40, null=True, blank=True)
+	STATUS = models.CharField(max_length=40, null=True, blank=True)
+	DETAILS = models.CharField(max_length=40, null=True, blank=True)
 
 class Rules(models.Model):
 	ID = models.AutoField(blank=False, primary_key=True)  
